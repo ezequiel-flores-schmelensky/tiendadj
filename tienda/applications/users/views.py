39159b22d0027ff04b1885f713f7972a54526675
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-21 10:31:23 -0500

    [feat] firebase login

--- tienda/applications/users/views.py
+++ tienda/applications/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class LoginUser(TemplateView):
+    template_name = "users/login.html"
