7554373b92bcb1813f46d35b72b0078343753c9c
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-21 11:10:08 -0500

    [feat] google login advance

--- tienda/applications/users/urls.py
+++ tienda/applications/users/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
         views.LoginUser.as_view(),
         name='login'
     ),
+    path(
+        'api/google-login/',
+        views.GoogleLoginView.as_view(),
+        name='users-google-login'
+    ),
 ]
--- tienda/applications/users/views.py
+++ tienda/applications/users/views.py
@@ -1,7 +1,19 @@
+from rest_framework.views import APIView
 from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
+from .serializers import LoginSocialSerializer
 
 
 class LoginUser(TemplateView):
     template_name = "users/login.html"
+
+
+class GoogleLoginView(APIView):
+    serializer_class = LoginSocialSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        id_token = serializer.data.get('token_id')
+
+        return None
