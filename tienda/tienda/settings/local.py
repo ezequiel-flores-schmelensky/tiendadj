406bd6932315a790f2876dc34565e25a6e23d557
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-22 09:50:35 -0500

    [feat] firebase configuration

--- tienda/tienda/settings/local.py
+++ tienda/tienda/settings/local.py
@@ -1,4 +1,6 @@
 from .base import *
+import firebase_admin
+from firebase_admin import credentials, auth
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -25,3 +27,6 @@ STATICFILES_DIRS = [BASE_DIR.child('static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('media')
+
+cred = credentials.RefreshToken('fbkey.json')
+default_app = firebase_admin.initialize_app(cred)
