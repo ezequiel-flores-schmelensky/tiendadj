8c27c878f5a8c4d44c16cea75179351bc0b0e01c
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-28 11:08:27 -0500

    [feat] sale process complete

--- tienda/applications/venta/views.py
+++ tienda/applications/venta/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -6,7 +7,8 @@ from rest_framework.generics import (
     ListAPIView,
     CreateAPIView,
 )
-from .models import Sale
+from applications.producto.models import Product
+from .models import Sale, SaleDetail
 from .serializers import VentaReporteSerializers, ProcesoVentaSerializer
 
 
@@ -28,6 +30,37 @@ class RegistrarVenta(CreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = ProcesoVentaSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        tipo_recibo = serializer.validated_data['type_invoice']
-        print('*******', tipo_recibo)
-        return Response({'code':'ok'})
+
+        venta = Sale.objects.create (
+            date_sale = timezone.now(),
+            amount = 0,
+            count = 0,
+            type_invoce = serializer.validated_data['type_invoce'],
+            type_payment = serializer.validated_data['type_payment'],
+            adreese_send = serializer.validated_data['adreese_send'],
+            user = self.request.user,
+        )
+
+        amount = 0
+        count = 0
+
+        productos = serializer.validated_data['productos']
+
+        for producto in productos:
+            prod = Product.objects.get(id=producto['pk'])
+            venta_detalle = SaleDetail(
+                sale=venta,
+                product=prod,
+                count=producto['count'],
+                price_purchase=prod.price_purchase,
+                price_sale=prod.price_sale,
+            )
+            amount = amount + prod.price_sale*producto['count']
+            count = count + producto['count']
+            ventas_detalle.append(venta_detalle)
+
+        venta.amount = amount
+        venta.count = count
+        venta.save()
+        SaleDetail.objects.bulk_create(ventas_detalle)
+        return Response({'msj':'venta exitosa'})
