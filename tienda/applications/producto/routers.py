38945ece8e5afc91512dc3b034ba670513be65ac
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-28 12:49:41 -0500

    [feat] viewsets post and get

---
+++ tienda/applications/producto/routers.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from . import viewsets
+
+
+router = DefaultRouter()
+router.register(r'colors', viewsets.ColorViewSet, basename="colors")
+urlpatterns = router.urls
--- tienda/tienda/urls.py
+++ tienda/tienda/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     re_path('', include('applications.users.urls')),
     re_path('', include('applications.producto.urls')),
     re_path('', include('applications.venta.urls')),
+    re_path('', include('applications.producto.routers')),
 ]
