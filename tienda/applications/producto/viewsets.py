47d687dad24eea09729fdd42f6dd98d26f36041e
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-28 13:22:46 -0500

    [feat] edit viewsets and perform

--- tienda/applications/producto/viewsets.py
+++ tienda/applications/producto/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import Colors, Product
 
 from .serializers import (
@@ -18,3 +19,13 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductoSerializerViewSet
     queryset = Product.objects.all()
     pagination_class = PaginationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(
+            video="https://youtu.be/h8a1gLl9C5A"
+        )
+
+    def list(self, request,*args, **kwargs):
+        queryset = Product.objects.productos_por_user(self.request.user)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
